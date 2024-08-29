f61039e57f43be9afa7244d5fa1a25b6d0181db6
Sachin k <112501723+sachinkz@users.noreply.github.com>
2024-08-29 14:54:37 +0530

    Update main.js

--- shared/main.js
+++ shared/main.js
@@ -160,7 +160,7 @@ class App {
     this.camera.matrixAutoUpdate = false;
 
     // Load the 3D model
-    window.gltfLoader.load("https://third-dimension.netlify.app/uploads/e1eb0222-949a-467c-a803-280238fe3273.glb", (gltf) => {
+    window.gltfLoader.load("https://third-dimension.netlify.app/uploads/5bce0e88-1c04-4f28-8a66-d59b97278eec.glb", (gltf) => {
       alert("loaded");
       // Find the main model in the loaded scene
       const model = gltf.scene.children.find(c => c.name === 'Sketchfab_model')
